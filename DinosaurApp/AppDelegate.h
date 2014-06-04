7daeead3c61a4596587921a6597c0749cb1ba841
thijs  verheul <thijsverheul@MacBook-Pro-van-thijs.local>
2014-06-04 15:03:31 +0200

    Initial Commit

---
+++ DinosaurApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DinosaurApp
+//
+//  Created by thijs  verheul on 04-06-14.
+//  Copyright (c) 2014 thijs  verheul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
